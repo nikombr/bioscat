d42fb66c1313cf3e91d068f848ead2dab1fae859
Nikoline Mai Bogely Rehn <nikoline@rehn.dk>
2024-09-10 20:19:58 +0200

    data

--- Functions/C_and_CUDA/gaussian_process.py
+++ Functions/C_and_CUDA/gaussian_process.py
@@ -2,6 +2,7 @@ import ctypes
 import numpy as np
 from scipy.io import savemat
 import os
+import time
 
 def gaussian_process(x, y, tau = 1, ell = 1):
 
@@ -51,10 +52,11 @@ def gaussian_process(x, y, tau = 1, ell = 1):
 if __name__ == "__main__":
     x = np.linspace(0,10,200)
     y = np.linspace(0,10,200)
-    #y = np.array([]);
+    y = np.array([]);
     ells = [1, 2, 4];
     taus = [0.25, 0.5, 1];
     for ell in ells:
         for tau in taus:
             Z = gaussian_process(x, y, tau = tau, ell = ell)
+            time.sleep(10)
     print("Hej fra Python")
