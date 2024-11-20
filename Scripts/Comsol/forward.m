4688a1a0faf709ea3e945f93ea6cfdadb94dd029
Nikoline Mai Bogely Rehn <nikoline@rehn.dk>
2024-11-20 17:28:32 +0100

    comsol

--- Scripts/Comsol/forward.m
+++ Scripts/Comsol/forward.m
@@ -7,7 +7,7 @@ format long
 
 % Load my solution
 lambda=325; % nm
-total_grid_points = 300;
+total_grid_points = 1000;
 beta = 0;
 protein_structure = 'demoleus2x2'; % Retinin2x2 demoleus2x2
 filename = sprintf("../../Results/forward/%s/near/fields_beta_%d_lambda_%d_num_segments_1_total_grid_points_%d.mat",protein_structure,beta,lambda,total_grid_points);
@@ -25,12 +25,16 @@ fs = load("../../Data/nanostructures/2D/demoleus2x2_f_300.txt");
 import com.comsol.model.*
 import com.comsol.model.util.*
 
+% Set location for temporary files
+-tmpdir $HOME/work3/s194146/comsoltmp
+-recoverydir $HOME/work3/s194146/comsolrecovery
+
 figure, plot(xs,fs), grid on, axis equal
 surface = [xs' fs'];
 save('surface.txt','surface','-ascii')
 
 % Load Comsol model
-model = mphload('forward61.mph');
+model = mphload('forward61_2.mph');
 
 % Set the parameter in the COMSOL model
 param_name = 'lambda_0';    % Name of the parameter in the COMSOL model
@@ -57,31 +61,52 @@ figure('Renderer', 'painters', 'Position', [400 400 1500 550]);
 tiledlayout(1,3,'TileSpacing','compact');
 nexttile;
 Ez_comsol = Ez_interp-E_background;
+<<<<<<< HEAD
+imagesc(x, y, abs(Ez_comsol));  % Absolute value of Ez for visualization
+=======
 imagesc(x, y, abs(Ez_comsol).^2);  % Absolute value of Ez for visualization
+>>>>>>> 6ef0007e1ccbb9c08ab322f0c9352dee59aea4cf
 title('Interpolated emw.Ez');
 xlabel('X-axis');
 ylabel('Y-axis');
 zlabel('|Ez|');
-grid on;
 axis square
 colorbar
 %clim([0,2])
 ax = gca;              % Get the current axes
+<<<<<<< HEAD
+ax.CLim = [0 0.5]; % Set the color limits
+
+nexttile;
+Ez_me = E_scat(:,:,3) + E_ref(:,:,3);
+%Ez_tot = E_scat(:,:,3)+E_inc(:,:,3);
+imagesc(x, y, abs(Ez_me));  % Absolute value of Ez for visualization
+=======
 ax.CLim = [0 0.4]; % Set the color limits
 
 nexttile;
 Ez_me = E_scat(:,:,3)+E_ref(:,:,3);
 %Ez_tot = E_scat(:,:,3)+E_inc(:,:,3);
 imagesc(x, y, abs(Ez_me).^2);  % Absolute value of Ez for visualization
+>>>>>>> 6ef0007e1ccbb9c08ab322f0c9352dee59aea4cf
 axis square
 colorbar
 %clim([0,2])
 ax = gca;              % Get the current axes
+<<<<<<< HEAD
+ax.CLim = [0 0.5]; % Set the color limits
+
+nexttile;
+imagesc(x, y, abs(Ez_comsol-Ez_me)); 
+=======
 ax.CLim = [0 0.4]; % Set the color limits
 
 nexttile;
 imagesc(x, y, abs(Ez_comsol-Ez_me).^2); 
+>>>>>>> 6ef0007e1ccbb9c08ab322f0c9352dee59aea4cf
 axis square
 colorbar
+ax = gca;              % Get the current axes
+ax.CLim = [0 0.5]; % Set the color limits
 
 exportgraphics(gcf,'hmm.png','Resolution',300);
